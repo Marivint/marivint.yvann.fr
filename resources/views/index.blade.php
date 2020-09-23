1b15d46f2b2f9c974c4a7425d31bb3fbb630ae0e
Yvann <marivint.yvann@gmail.com>
2020-09-23 18:02:41 +0200

    CSS Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,20 @@
 
 @section('content')
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
+    <section id="main-header" class="row">
+        <div id="main-header-text" class="col-md-6 offset-md-1">
+        	<div class="subtitle">Yvann Marivint</div>
+        	<br>
+          <h1>
+        		Bonjour, my 
+        		<br> name's Yvann
+        		<br> I'm a Web Developper
+                <br> & project manager.
+        	</h1>
+        </div>
+        <div class="col-md-5">
             
         </div>
-    </div>
+    </section>
 </div>
 @endsection
